a844f392d1996eb9fd09b57cf7ea88fd9a5625fa
Jeffrey Lin <jeffreylin7411@gmail.com>
2022-01-03 23:38:18 -0500

    added search bar

--- movie-app/src/components/Home.js
+++ movie-app/src/components/Home.js
@@ -1,10 +1,13 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 
 import { useHomeFetch } from '../hooks/useHomeFetch';
 import { Image_URL } from '../API';
 import FrontImage from './FrontImage';
 import Grid from './Grid';
 import Thumbnails from './Thumbnails';
+import SearchBar from './SearchBar';
+import Spinner from './Spinner';
+
 import NoImage from '../images/no_image.jpg';
 
 const poster_size = Image_URL.poster_size;
@@ -14,17 +17,18 @@ const URL = Image_URL.URL;
 
 
 const Home = () => {
-    const {state, loading, error} = useHomeFetch();
+    const {state, loading, error, searchTerm, setSearchTerm} = useHomeFetch();
     return (
         <>  
-            {state.results[0] ? (
+            {!searchTerm && state.results[0] ? (
                 <FrontImage
                 image={`${URL}${Backdrop_size}${state.results[0].backdrop_path}`}
                 title={state.results[0].original_title}
                 text={state.results[0].overview}
                 />
             ) : null}
-            <Grid header = 'Popular Movies'>
+            <SearchBar setSearchTerm={setSearchTerm}/>
+            <Grid header = {searchTerm ? 'Search Results' : 'Popular Movies'}>
                 {state.results.map(movie => (
                     <Thumbnails
                         key={movie.id}
--- movie-app/src/hooks/useHomeFetch.js
+++ movie-app/src/hooks/useHomeFetch.js
@@ -9,6 +9,7 @@ const initialState = {
 };
 
 export const useHomeFetch = () => {
+    const [searchTerm, setSearchTerm] = useState('');
     const [state, setState] = useState(initialState);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
@@ -29,9 +30,11 @@ export const useHomeFetch = () => {
         setLoading(false);
     }
 
-    useEffect(() => {
-        getMovies(1)
-    }, [])
 
-    return {state, loading, error}
+    useEffect(() => {
+        setState(initialState)
+        getMovies(1, searchTerm);
+    }, [searchTerm])
+
+    return {state, loading, error, searchTerm, setSearchTerm}
 }
